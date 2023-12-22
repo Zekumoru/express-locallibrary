95ad43bb11e2d3686486a161174bdbc8443d9943
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2023-12-22 22:33:00 +0100

    Create db schema and models

---
+++ models/bookinstance.ts
@@ -0,0 +1,19 @@
+import { Schema, model } from 'mongoose';
+
+const BookInstanceSchema = new Schema({
+  book: { type: Schema.Types.ObjectId, ref: 'Book', required: true },
+  imprint: { type: String, required: true },
+  status: {
+    type: String,
+    required: true,
+    enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'],
+    default: 'Maintenance',
+  },
+  due_back: { type: Date, default: Date.now() },
+});
+
+BookInstanceSchema.virtual('url').get(function () {
+  return `/catalog/bookinstance/${this._id}`;
+});
+
+export default model('BookInstance', BookInstanceSchema);
