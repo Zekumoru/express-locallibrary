fb116ec75fac23ca9f26a8a27eaf0e570b3235c3
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2023-12-25 01:07:20 +0100

    Format date to cleaner form using luxon

--- models/bookinstance.ts
+++ models/bookinstance.ts
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose';
+import { DateTime } from 'luxon';
 
 const BookInstanceSchema = new Schema({
   book: { type: Schema.Types.ObjectId, ref: 'Book', required: true },
@@ -14,6 +15,10 @@ const BookInstanceSchema = new Schema({
 
 BookInstanceSchema.virtual('url').get(function () {
   return `/catalog/bookinstance/${this._id}`;
+});
+
+BookInstanceSchema.virtual('due_back_formatted').get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 export default model('BookInstance', BookInstanceSchema);
