11f1c4fac582b5cb5a51e7cd7b211403cf59db3f
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2023-12-25 15:18:49 +0100

    Create author form page

--- controllers/authorController.ts
+++ controllers/authorController.ts
@@ -1,7 +1,8 @@
-import { Request } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import asyncHandler from 'express-async-handler';
 import Author from '../models/author';
 import Book from '../models/book';
+import { body, validationResult } from 'express-validator';
 
 export interface AuthorRequest
   extends Request<{
@@ -45,18 +46,70 @@ export const author_detail = asyncHandler(
 );
 
 // Display Author create form on GET.
-export const author_create_get = asyncHandler(
-  async (req: AuthorRequest, res, next) => {
-    res.send('NOT IMPLEMENTED: Author create GET');
-  }
-);
+export const author_create_get = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res.render('author_form', { title: 'Create Author' });
+};
 
 // Handle Author create on POST.
-export const author_create_post = asyncHandler(
-  async (req: AuthorRequest, res, next) => {
-    res.send('NOT IMPLEMENTED: Author create POST');
-  }
-);
+export const author_create_post = [
+  // Validate and sanitize fields.
+  body('first_name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('First name must be specified.')
+    .isAlphanumeric()
+    .withMessage('First name has non-alphanumeric characters.'),
+  body('family_name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Family name must be specified.')
+    .isAlphanumeric()
+    .withMessage('Family name has non-alphanumeric characters.'),
+  body('date_of_birth', 'Invalid date of birth')
+    .optional({ values: 'falsy' })
+    .isISO8601()
+    .toDate(),
+  body('date_of_death', 'Invalid date of death')
+    .optional({ values: 'falsy' })
+    .isISO8601()
+    .toDate(),
+
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create Author object with escaped and trimmed data
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      return res.render('author_form', {
+        title: 'Create Author',
+        author: author,
+        errors: errors.array(),
+      });
+    }
+
+    // Data from form is valid.
+
+    // Save author.
+    await author.save();
+    // Redirect to new author record.
+    res.redirect(author.url);
+  }),
+];
 
 // Hnadle Author delete form on GET
 export const author_delete_get = asyncHandler(
--- models/author.ts
+++ models/author.ts
@@ -1,40 +1,53 @@
 import { Schema, model } from 'mongoose';
 import { DateTime } from 'luxon';
 
-const AuthorSchema = new Schema({
-  first_name: { type: String, required: true, maxLength: 100 },
-  family_name: { type: String, required: true, maxLength: 100 },
-  date_of_birth: { type: Date },
-  date_of_death: { type: Date },
-});
+const AuthorSchema = new Schema(
+  {
+    first_name: { type: String, required: true, maxLength: 100 },
+    family_name: { type: String, required: true, maxLength: 100 },
+    date_of_birth: { type: Date },
+    date_of_death: { type: Date },
+  },
+  {
+    virtuals: {
+      url: {
+        get() {
+          return `/catalog/author/${this._id}`;
+        },
+      },
+      name: {
+        get() {
+          let fullname = '';
 
-AuthorSchema.virtual('name').get(function () {
-  let fullname = '';
+          if (this.first_name && this.family_name) {
+            fullname = `${this.family_name}, ${this.first_name}`;
+          }
 
-  if (this.first_name && this.family_name) {
-    fullname = `${this.family_name}, ${this.first_name}`;
+          return fullname;
+        },
+      },
+      date_of_living: {
+        get() {
+          if (!this.date_of_birth && !this.date_of_death) {
+            return 'Unknown';
+          }
+
+          const dateOfBirth = this.date_of_birth
+            ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+                DateTime.DATE_MED
+              )
+            : 'Unknown';
+          const dateOfDeath = this.date_of_death
+            ? DateTime.fromJSDate(this.date_of_death).toLocaleString(
+                DateTime.DATE_MED
+              )
+            : 'Alive';
+
+          return `${dateOfBirth} - ${dateOfDeath}`;
+        },
+      },
+    },
   }
-
-  return fullname;
-});
-
-AuthorSchema.virtual('url').get(function () {
-  return `/catalog/author/${this._id}`;
-});
-
-AuthorSchema.virtual('date_of_living').get(function () {
-  if (!this.date_of_birth && !this.date_of_death) {
-    return 'Unknown';
-  }
-
-  const dateOfBirth = this.date_of_birth
-    ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
-    : 'Unknown';
-  const dateOfDeath = this.date_of_death
-    ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
-    : 'Alive';
-
-  return `${dateOfBirth} - ${dateOfDeath}`;
-});
+);
 
 export default model('Author', AuthorSchema);
