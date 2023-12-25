77fdfe738dfe2c6beb9425d4952ef30d61ef9330
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2023-12-25 16:38:01 +0100

    Create bookinstance form page

--- controllers/bookinstanceController.ts
+++ controllers/bookinstanceController.ts
@@ -1,6 +1,8 @@
 import { Request } from 'express';
 import BookInstance from '../models/bookinstance';
 import asyncHandler from 'express-async-handler';
+import { body, validationResult } from 'express-validator';
+import Book from '../models/book';
 
 export interface BookInstanceRequest
   extends Request<{
@@ -41,18 +43,61 @@ export const bookinstance_detail = asyncHandler(
 );
 
 // Display BookInstance create form on GET.
-export const bookinstance_create_get = asyncHandler(
-  async (req: BookInstanceRequest, res, next) => {
-    res.send('NOT IMPLEMENTED: BookInstance create GET');
-  }
-);
+export const bookinstance_create_get = asyncHandler(async (req, res, next) => {
+  const allBooks = await Book.find({}, 'title').sort({ title: 1 }).exec();
+
+  res.render('bookinstance_form', {
+    title: 'Create BookInstance',
+    book_list: allBooks,
+  });
+});
 
 // Handle BookInstance create on POST.
-export const bookinstance_create_post = asyncHandler(
-  async (req: BookInstanceRequest, res, next) => {
-    res.send('NOT IMPLEMENTED: BookInstance create POST');
-  }
-);
+export const bookinstance_create_post = [
+  // Validate and sanitize fields.
+  body('book', 'Book must be specified').trim().isLength({ min: 1 }).escape(),
+  body('imprint', 'Imprint must be specified')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('status').escape(),
+  body('due_back', 'Invalid date')
+    .optional({ values: 'falsy' })
+    .isISO8601()
+    .toDate(),
+
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a BookInstance object with escaped and trimmed data.
+    const bookInstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors.
+      // Render form again with sanitized values and error messages.
+      const allBooks = await Book.find({}, 'title').sort({ title: 1 }).exec();
+
+      return res.render('bookinstance_form', {
+        title: 'Create BookInstance',
+        book_list: allBooks,
+        selected_book: bookInstance.book._id,
+        errors: errors.array(),
+        bookinstance: bookInstance,
+      });
+    }
+
+    // Data from form is valid
+    await bookInstance.save();
+    res.redirect(bookInstance.url);
+  }),
+];
 
 // Display BookInstance delete form on GET.
 export const bookinstance_delete_get = asyncHandler(
--- models/bookinstance.ts
+++ models/bookinstance.ts
@@ -1,24 +1,39 @@
 import { Schema, model } from 'mongoose';
 import { DateTime } from 'luxon';
 
-const BookInstanceSchema = new Schema({
-  book: { type: Schema.Types.ObjectId, ref: 'Book', required: true },
-  imprint: { type: String, required: true },
-  status: {
-    type: String,
-    required: true,
-    enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'],
-    default: 'Maintenance',
+const BookInstanceSchema = new Schema(
+  {
+    book: { type: Schema.Types.ObjectId, ref: 'Book', required: true },
+    imprint: { type: String, required: true },
+    status: {
+      type: String,
+      required: true,
+      enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'],
+      default: 'Maintenance',
+    },
+    due_back: { type: Date, default: Date.now },
   },
-  due_back: { type: Date, default: Date.now },
-});
-
-BookInstanceSchema.virtual('url').get(function () {
-  return `/catalog/bookinstance/${this._id}`;
-});
-
-BookInstanceSchema.virtual('due_back_formatted').get(function () {
-  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
-});
+  {
+    virtuals: {
+      url: {
+        get() {
+          return `/catalog/bookinstance/${this._id}`;
+        },
+      },
+      due_back_formatted: {
+        get() {
+          return DateTime.fromJSDate(this.due_back).toLocaleString(
+            DateTime.DATE_MED
+          );
+        },
+      },
+      due_back_yyyy_mm_dd: {
+        get() {
+          return DateTime.fromJSDate(this.due_back).toISODate(); // format 'YYYY-MM-DD'
+        },
+      },
+    },
+  }
+);
 
 export default model('BookInstance', BookInstanceSchema);
